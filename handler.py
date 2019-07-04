ca7e0445f3ac46b5e5bf2689003f24ca7829cf41
Hersel Giannella <hersel1991@gmail.com>
2019-07-04 10:50:37 +0200

    Add files via upload

---
+++ handler.py
@@ -0,0 +1,18 @@
+import random
+import config
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+
+#WELCOME
+def welcome(bot, update):
+	for new in update.message.new_chat_members:
+		update.message.reply_text('Welcome here {username} in {chat_title}'
+			.format(username=update.message.from_user.first_name, chat_title=update.message.chat.title))
+		
+#DICHIARAZIONE FUNZIONI
+def init(bot, update):
+	welcome(bot, update)
+	
+	
+
+
---
+++ welcome.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import config, handler
+
+# Questo abilita i log
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
+def error(update, context):
+    logger.warning('Update "%s" genera errore: "%s"', update, context.error)
+
+# Questa è la funzione che inizializza il bot
+def main():
+    updater = Updater(config.bot_token)
+    dp = updater.dispatcher
+    # Qui richiamo le funzioni senza comando, =>handler
+    dp.add_handler(MessageHandler(None, handler.init))
+    dp.add_error_handler(error)
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
